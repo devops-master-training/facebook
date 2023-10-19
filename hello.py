74abe27dbc2946bbcc7d8c9ea2a530bff5e59765
vasavi <vasavi@gmail.com>
2023-10-19 14:50:14 +0530

    Revert "added line in hello.py"
    This reverts commit 9838ad806faff54b2245aae64ee11f3607655b4d.

--- hello.py
+++ hello.py
@@ -1,3 +1,6 @@
 print("testing github and git commands")
+<<<<<<< HEAD
 print("git-prctice by creating facebook project")
 print("hi there"))
+=======
+>>>>>>> parent of 9838ad8 (added line in hello.py)
